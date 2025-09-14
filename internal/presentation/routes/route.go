481b698ce8950b61e44bb4f7c8200adbb2bea0e9
Kian <kianyarii1383@gmail.com>
2025-09-15 03:28:06 +0330

    feat: admin auth

--- internal/presentation/routes/route.go
+++ internal/presentation/routes/route.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/FamCan-RiskAssessment/Backend/internal/domain/enum"
 	"github.com/FamCan-RiskAssessment/Backend/wire"
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +11,12 @@ func Run(ginEngine *gin.Engine, app *wire.Application) {
 	ginEngine.Use(app.Middlewares.Localization.Localization)
 	ginEngine.Use(app.Middlewares.Recovery.Recovery)
 	SetupGeneralRoutes(ginEngine.Group("/"), app)
-	SetupAdminRoutes(ginEngine.Group("/admin"), app)
+	registerAdminRoutes(ginEngine.Group("/admin"), app)
 	SetupCustomerRoutes(ginEngine.Group("/"), app)
+}
+
+func registerAdminRoutes(ginEngine *gin.RouterGroup, app *wire.Application) {
+	ginEngine.Use(app.Middlewares.Auth.AuthRequired)
+	ginEngine.Use(app.Middlewares.Auth.RequiredWithPermission([]enum.PermissionType{enum.PermissionAll}))
+	SetupAdminRoutes(ginEngine, app)
 }

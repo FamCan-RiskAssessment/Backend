4d4dd80c6e23f069c8836e940773533c6e819207
Kian <kianyarii1383@gmail.com>
2025-09-15 03:13:46 +0330

    tmp: deployment

--- internal/presentation/middleware/cors.go
+++ internal/presentation/middleware/cors.go
@@ -15,12 +15,12 @@ func NewCorsMiddleware() *CORSMiddleware {
 
 func (cm *CORSMiddleware) CORS() gin.HandlerFunc {
 	corsConfig := cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://185.231.115.28:5173"},
-		AllowMethods:     []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "ngrok-skip-browser-warning"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization", "ngrok-skip-browser-warning"},
+		ExposeHeaders: []string{"Content-Length"},
+		// AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
 	}
 
 	return cors.New(corsConfig)

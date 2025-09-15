b2a44bd948d8eee7959df023bbbfc6cad9922194
Kian <kianyarii1383@gmail.com>
2025-09-15 03:53:12 +0330

    tmp: deployment

--- internal/presentation/middleware/cors.go
+++ internal/presentation/middleware/cors.go
@@ -15,21 +15,11 @@ func NewCorsMiddleware() *CORSMiddleware {
 
 func (cm *CORSMiddleware) CORS() gin.HandlerFunc {
 	corsConfig := cors.Config{
-		AllowOriginFunc: func(origin string) bool {
-			// Allow localhost for development
-			if origin == "http://localhost:5173" || origin == "http://localhost:3000" {
-				return true
-			}
-			// Allow your production origin
-			if origin == "http://185.231.115.28:5173" {
-				return true
-			}
-			return false
-		},
+		AllowOrigins:     []string{"http://185.231.115.28:5173", "http://localhost:5173"},
 		AllowMethods:     []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "ngrok-skip-browser-warning"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: false,
+		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}
 

363e8df905c44eec7e9dd700833f741f392de816
Kian <kianyarii1383@gmail.com>
2025-10-12 04:49:33 +0330

    fix: admin permissions

--- internal/presentation/routes/admin.go
+++ internal/presentation/routes/admin.go
@@ -45,6 +45,7 @@ func SetupAdminRoutes(routerGroup *gin.RouterGroup, app *wire.Application) {
 
 	userManagement := routerGroup.Group("/users")
 	{
+		userManagement.Use(app.Middlewares.Auth.RequiredWithPermission([]enum.PermissionType{enum.PermissionType(enum.CategoryPatientManagement)}))
 		userManagement.GET("", app.Controllers.Admin.UserController.GetUsers)
 	}
 
